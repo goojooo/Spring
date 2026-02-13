c5f6ef4cfc150fefae669e46ddf23c30d3db9050
Nayan.Dahare <nayandahare02@gmail.com>
2026-02-13 17:34:37 +0530

    spring practice

--- SpringProgram3/src/main/Main.java
+++ SpringProgram3/src/main/Main.java
@@ -1,11 +1,17 @@
 package main;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import beans.Student;
 
 public class Main {
 	public static void main(String args[])
 	{
-		ApplicationContext = new ClassPathXmlApplicationContext();
+		String rfs = "/resources/applicationContext.xml";
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(rfs);
+		Student std = (Student)ctx.getBean("student");
+		std.display();
 	}
 
 }
