5a77a6ffb7c66e535d989493a1dc76597521c80e
Nayan.Dahare <nayandahare02@gmail.com>
2026-02-13 18:11:50 +0530

    spring practice

--- SpringProgram3.2/src/main/Main.java
+++ SpringProgram3.2/src/main/Main.java
@@ -1,15 +1,15 @@
 package main;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import beans.Student;
-import resources.SpringConfigFile;
 
 public class Main {
 	public static void main(String args[])
 	{
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfigFile.class);
+		String rfs = "/resources/applicationContext.xml";
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(rfs);
 		Student std = (Student)ctx.getBean("student");
 		std.display();
 	}
