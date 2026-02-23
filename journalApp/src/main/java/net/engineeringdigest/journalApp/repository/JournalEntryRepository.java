cf0b7d27682a19abfee707f8eca7cda2b185574e
Nayan.Dahare <nayandahare02@gmail.com>
2026-02-23 16:55:42 +0530

    spb

---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,7 @@
+package net.engineeringdigest.journalApp.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends  MongoRepository {
+
+}
